36e1d976b580ddf83ab907696ab27e357a7a3b0e
Nathan <npersall@gmail.com>
2019-02-04 17:18:13 -0800

    Fills board and creates state

--- src/App.js
+++ src/App.js
@@ -3,30 +3,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+
+      board : Array(9).fill("X")
+    
+    }
+  }
+
   render() {
+      
+    const Box =this.state.board.map(box => <div className="box">{box}</div>)
+
     return (
       <div className="container">
          <h1>Tic Tac Toe App</h1>
          
          <div className="board">
 
-          <div className= "box"></div>
-         
-          <div className= "box"></div>
-
-          <div className= "box"></div>
-
-          <div className= "box"></div>
-
-          <div className= "box"></div>
-
-          <div className= "box"></div>
-
-          <div className= "box"></div>
-
-          <div className= "box"></div>
-
-          <div className= "box"></div>
+          {Box}
 
         </div>
 
